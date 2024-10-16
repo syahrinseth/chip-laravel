1fa2f813a64229b192cb82c324dd0026c96ae0c8
syahrinseth <syahrinsethservices@gmail.com>
2024-10-16 16:07:02 +0800

    added Chip Service Provider

--- src/ChipServiceProvider.php
+++ src/ChipServiceProvider.php
@@ -6,18 +6,19 @@ use Illuminate\Support\ServiceProvider;
 
 class ChipServiceProvider extends ServiceProvider
 {
-    public function register()
-    {
-        $this->app->bind('chipapi', function () {
-            $config = config('chipapi');
-            return new \Chip\ChipApi($config['brand_id'], $config['api_key'], $config['endpoint']);
-        });
-    }
-
     public function boot()
     {
+        // Load the package's configuration file
+        $this->mergeConfigFrom(__DIR__.'/../config/chipapi.php', 'chipapi');
+
+        // Optionally, publish the configuration file
         $this->publishes([
-            __DIR__ . '/../config/chipapi.php' => config_path('chipapi.php'),
-        ]);
+            __DIR__.'/../config/chipapi.php' => config_path('chipapi.php'),
+        ], 'config');
+    }
+
+    public function register()
+    {
+        // Register bindings or services for your package
     }
 }
